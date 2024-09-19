cbbab84879d4bc50c497e1ca687cd2c0042b3527
Jude Ma <dragonscripter03@gmail.com>
2024-09-19 17:52:35 -0400

    Added Permission and role classes as well adding the fourth project in asp.net core web api for the use of restful api

---
+++ Authentication/Model/Permission.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Model
+{
+    public static class Permission
+    {
+        public const string Create = "Create";
+        public const string Read = "Read";
+        public const string Update = "Update";
+        public const string Delete = "Delete";
+
+        private static readonly Dictionary<string, List<string>> RolePermission = new Dictionary<string, List<string>>
+        {
+            {"Admin", new List<string>
+                {
+                    Create,
+                    Read,
+                    Update,
+                    Delete
+                }
+            },
+            {"Editor", new List<string>
+                {
+                    Read,
+                    Update
+                }
+            },
+            {"Contributor", new List<string>
+                {
+                    Create,
+                    Read
+                }
+            },
+            {"Subscriber", new List<string>
+                {
+                    Read
+                }
+            
+            }
+        };
+
+    }
+}
