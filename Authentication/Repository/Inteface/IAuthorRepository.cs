8f168c995cc0572e8daf88a3b8f16e9525f3eccc
Jude Ma <dragonscripter03@gmail.com>
2024-09-21 16:35:24 -0400

    Changed the arguments for the publish method

--- Authentication/Repository/Inteface/IAuthorRepository.cs
+++ Authentication/Repository/Inteface/IAuthorRepository.cs
@@ -18,7 +18,7 @@ namespace Authentication.Repository.Inteface
         Task<bool> CreateContentAsync(Content content);
         Task<bool> GetContentByIdAsync(int contentId);
         Task<IEnumerable<Content>> GetAllContentAsync();
-        Task<bool> PublishContent(Content content);
+        Task<bool> PublishContent(int contentID, string authorID );
         Task<bool> hasPermissionAsync(string userID, string perm);
     }
 }
