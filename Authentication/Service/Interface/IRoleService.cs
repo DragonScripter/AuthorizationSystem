e23166bcbdabcf31fe1cda1a09a0443f4a8d9561
Jude Ma <dragonscripter03@gmail.com>
2024-09-21 15:19:12 -0400

    changed the type to Claim from the previous <IEnumerable<bool>> in role interface service

--- Authentication/Service/Interface/IRoleService.cs
+++ Authentication/Service/Interface/IRoleService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,7 @@ namespace Authentication.Service.Interface
     public interface IRoleService
     {
         Task<bool> addClaimToRoleAsync(string roles, string claimType, string value);
-        Task <IEnumerable<bool>> GetClaimsForRolesAsync(string roles);
+        Task <IEnumerable<Claim>> GetClaimsForRolesAsync(string roles);
         Task<bool> CheckPermForRolesAsync(string roles, string claimType, string Value);
     }
 }
