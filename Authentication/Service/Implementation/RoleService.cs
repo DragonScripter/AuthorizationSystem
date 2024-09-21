b30be311b277a8d5035802af6fdefa1d7dba1a7c
Jude Ma <dragonscripter03@gmail.com>
2024-09-21 14:39:55 -0400

    Created the role service interface and added the method names

---
+++ Authentication/Service/Implementation/RoleService.cs
@@ -0,0 +1,20 @@
+﻿using Authentication.Model;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Service
+{
+    public class RoleService
+    {
+        RoleManager<RoleEF> _roleManager;
+
+        public RoleService(RoleManager<RoleEF> roleManager) 
+        {
+            _roleManager = roleManager;
+        }
+    }
+}
---
+++ Authentication/Service/Interface/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Service.Interface
+{
+    public interface IRoleService
+    {
+        Task<bool> addClaimToRoleAsync(string role, string claim, string type);
+        Task<bool> PutClaimsToRoleAsync();
+    }
+}
