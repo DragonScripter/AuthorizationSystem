b3f5904e37fe9e6eefa8eb9cc3de477eb38b7cdd
Jude Ma <dragonscripter03@gmail.com>
2024-09-21 16:16:15 -0400

    Created the user Auth method for the author role

--- Authentication/Repository/Implementation/AuthorRepository.cs
+++ Authentication/Repository/Implementation/AuthorRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Authentication.Data;
+using Authentication.Model;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,30 @@ using System.Threading.Tasks;
 
 namespace Authentication.Repository.Implementation
 {
-    internal class AuthorRepository
+    public class AuthorRepository
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<RoleEF> _roleManager;
+        private readonly AppDbContext _context;
+
+        public AuthorRepository(UserManager<AppUser> userManager, RoleManager<RoleEF> roleManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        public async Task<bool> userAuthAsync(string name, string password) 
+        {
+            var user = await _userManager.FindByNameAsync(name);
+            if (user == null) 
+            {
+                throw new Exception("User not found.");
+            }
+
+            var key = await _userManager.CheckPasswordAsync(user, password);
+            return true;
+        }
+
     }
 }
