c2843e685ff65fbd495b75d5ecb22d08d4a0d939
Jude Ma <dragonscripter03@gmail.com>
2024-09-24 21:25:51 -0400

    Deleted default files and created the controller for the auth as well as the login

---
+++ AuthentificationSystem/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using Authentication.Model;
+using AuthentificationSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthentificationSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[Auth]")]
+    public class AuthController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AuthController(UserManager<AppUser> userManager) 
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = _userManager.FindByNameAsync(loginModel.Username);
+            if (user == null) 
+            {
+                return NotFound("Not Found");
+            }
+
+            var passwordCheck = await _userManager.CheckPasswordAsync(user, loginModel.Password);
+            if (!passwordCheck) 
+            {
+                return Unauthorized("Invalid login attempt");
+            }
+
+            return Ok("Login sucess");
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
