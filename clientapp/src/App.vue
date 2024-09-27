7d9fbcb6b34e69ebd218dd4206120ec0889d76cd
Jude Ma <dragonscripter03@gmail.com>
2024-09-27 00:57:28 -0400

    Added bootstrap and completed the login page design and fetching the api call

--- clientapp/src/App.vue
+++ clientapp/src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
     import { defineComponent } from 'vue';
     import Login from './components/Login.vue'; // Import your Login component
-    import HelloWorld from './components/HelloWorld.vue'; // Ensure you have this imported
-    import TheWelcome from './components/TheWelcome.vue'; // Ensure you have this imported
 </script>
 
 <template>
@@ -12,31 +10,3 @@
     </main>
 </template>
 
-<style scoped>
-    header {
-        line-height: 1.5;
-    }
-
-    .logo {
-        display: block;
-        margin: 0 auto 2rem;
-    }
-
-    @media (min-width: 1024px) {
-        header {
-            display: flex;
-            place-items: center;
-            padding-right: calc(var(--section-gap) / 2);
-        }
-
-        .logo {
-            margin: 0 2rem 0 0;
-        }
-
-        header .wrapper {
-            display: flex;
-            place-items: flex-start;
-            flex-wrap: wrap;
-        }
-    }
-</style>
