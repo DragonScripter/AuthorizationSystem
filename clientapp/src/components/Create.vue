e2847aeca5df2c3fcd1571619d9b3dbec30a20d9
Jude Ma <dragonscripter03@gmail.com>
2024-10-05 19:52:07 -0400

    Finished making the basic front of the page

--- clientapp/src/components/Create.vue
+++ clientapp/src/components/Create.vue
@@ -3,6 +3,27 @@
         <div class="content">
             <h1>Create post</h1>
         </div>
+        <form @submit.prevent="handleSubmit">
+            <div class="group">
+                <label for="title"><strong>Title</strong></label>
+                <input type="text"
+                       v-model="formData.title"
+                       id="title"
+                       placeholder="Enter a title"
+                       class="input-box"
+                       required />
+            </div>
+            <div class="group">
+                <label for="body"><strong>Body</strong></label>
+                <input type="text"
+                       v-model="formData.body"
+                       id="body"
+                       placeholder="Enter a description"
+                       class="input-box"
+                       required />
+                <button type="submit" class="btn btn-primary custom">Post</button>
+            </div>
+        </form>
     </div>
 </template>
 
