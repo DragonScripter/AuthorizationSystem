01b1ff7e46c7f8a0a3f2a9e29bbc7a7cfdb1c90d
Jude Ma <dragonscripter03@gmail.com>
2024-10-04 23:47:04 -0400

    Made the script for the index vue

--- clientapp/src/components/Index.vue
+++ clientapp/src/components/Index.vue
@@ -2,10 +2,29 @@
     <div class="container">
         <h1>Main</h1>
         <div class="posts" v-for="post in posts" :key="post.id">
-
+            <h2 class="post-title" @click="goToPost(post.id)">{{ post.title }}</h2>
+            <p class="post-snippet">{{ post.snippet }}</p>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                posts: [
+                    { id: 1, title: 'Post 1', snippet: 'This is a snippet of post 1.' },
+                    { id: 2, title: 'Post 2', snippet: 'This is a snippet of post 2.' },
+                ],
+            };
+        },
+        methods: {
+            goToPost(postId) {
+                this.$router.push({ name: 'PostDetail', params: { postId } });
+            },
+        },
+    };
+</script>
 
 
 
@@ -24,6 +43,7 @@
         justify-content: center;
         align-items: center;
     }
+
     .container {
         height: 100vh;
         width: 100vw;
