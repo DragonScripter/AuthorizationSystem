61c85b78e66a12a53ac55a4d6e910315f51558ec
Jude Ma <dragonscripter03@gmail.com>
2024-10-05 20:13:03 -0400

    finished the script for the form

--- clientapp/src/components/Create.vue
+++ clientapp/src/components/Create.vue
@@ -23,11 +23,59 @@
                        required />
                 <button type="submit" class="btn btn-primary custom">Post</button>
             </div>
+            <div v-if="formData.errorMessage" class="error">
+                {{ formData.errorMessage }}
+            </div>
         </form>
     </div>
 </template>
 
-<script>
+<script lang="ts">
+    import { defineComponent, reactive } from 'vue'
+
+    export default defineComponent({
+        setup() {
+            const formData = reactive({
+                title: '',
+                body: '',
+                errorMessage: ''
+            });
+
+            const handleSubmit = async () => {
+                if (formData.title === '' || formData.body === '')
+                {
+                    formData.errorMessage = 'Please fill in all the fields.';
+                }
+                else
+                {
+                    try
+                    {
+                        const response = await fetch('https://localhost:7049/api/auth/create', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                title: formData.title,
+                                body: formData.body,
+                            }),
+                        });
+                        if (!response.ok) {
+                            throw new Error('Create failed');
+                        }
+
+                        const data = await response.json();
+                        console.log('Create sucess!', data);
+                    }
+                    catch(error)
+                    {
+                        formData.errorMessage = 'Create failed';
+                        console.log(error);
+                    }
+                }
+            }
+        },
+    });
 
 </script>
 
