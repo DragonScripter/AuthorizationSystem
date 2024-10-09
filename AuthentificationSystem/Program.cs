71349159e4ec4f51cbf1773f5838ff525adee8a0
Jude Ma <dragonscripter03@gmail.com>
2024-10-09 16:53:36 -0400

    Added the auth service and the secret key

--- AuthentificationSystem/Program.cs
+++ AuthentificationSystem/Program.cs
@@ -2,11 +2,14 @@ using Authentication.Data;
 using Authentication.Model;
 using Authentication.Service.Implementation;
 using Authentication.Service.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System.Drawing.Text;
+using System.Text;
 
 
 
@@ -14,6 +17,24 @@ using System.Drawing.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "Server",
+        ValidAudience = "User",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET"))),
+
+    };
+});
 void ConfigureServices(IServiceCollection services)
 {
     services.AddDbContext<AppDbContext>(options =>
