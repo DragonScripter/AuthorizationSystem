391279d15d61e90de64b59bff3dc8c27839c5402
DragonScripter <108528432+DragonScripter@users.noreply.github.com>
2024-12-20 13:13:44 -0500

    Update AppDbContextFactory.cs

--- Authentication/Data/AppDbContextFactory.cs
+++ Authentication/Data/AppDbContextFactory.cs
@@ -11,13 +11,7 @@ namespace Authentication.Data
 {
     public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
-        public AppDbContext CreateDbContext(string[]? args = null)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=DataDB;Trusted_Connection=True;");
-
-                return new AppDbContext(optionsBuilder.Options);
-        }
+        
 
     }
 }
